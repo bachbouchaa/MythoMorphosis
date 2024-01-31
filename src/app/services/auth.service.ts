e1bc18a3ee495c090a8575c69755f3ec39f2d185
bachbouchaa <nermine.bacha@insat.ucar.tn>
2024-01-31 19:27:36 +0100

    interceptor

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Router } from '@angular/router';
+import {JwtHelperService} from '@auth0/angular-jwt'
+import { TokenApiModel } from '../models/token-api';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private link : string ='';
+  private userPayload:any;
   constructor (private http: HttpClient, private router: Router){
+    this.userPayload = this.decodedToken();
+
   }
   signUp(userObj:any){
     return this.http.post<any>(`${this.link}register`, userObj);
@@ -21,23 +27,38 @@ export class AuthService {
   storeToken(tokenValue: string){
     localStorage.setItem('token', tokenValue)
   }
-  storeRefreshToken(tokenValue: string){
-    localStorage.setItem('refreshToken', tokenValue)
-  }
 
   getToken(){
     return localStorage.getItem('token')
   }
+
+  isAuthenticated(): boolean{
+    return !!localStorage.getItem('token')
+  }
+  decodedToken(){
+    const jwtHelper = new JwtHelperService();
+    const token = this.getToken()!;
+    console.log(jwtHelper.decodeToken(token))
+    return jwtHelper.decodeToken(token)
+  }
+  getfullNameFromToken(){
+    if(this.userPayload)
+    return this.userPayload.name;
+  }
+
+  getRoleFromToken(){
+    if(this.userPayload)
+    return this.userPayload.role;
+  }
+  storeRefreshToken(tokenValue: string){
+    localStorage.setItem('refreshToken', tokenValue)
+  }
   getRefreshToken(){
     return localStorage.getItem('refreshToken')
   }
-
-  isAuthenticated(): boolean{
-    //convert string to boolean
-    return !!localStorage.getItem('token')
+  renewToken(tokenApi : TokenApiModel){
+    return this.http.post<any>(`${this.link}refresh`, tokenApi)
   }
-  
-
 }
 
 
